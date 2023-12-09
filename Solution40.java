c40e50a5bbb4ab988feac6570e9b11a7276547e8
Vishal Shinde <95472421+Shinde2375@users.noreply.github.com>
2023-12-09 10:18:45 +0530

    Add files via upload
    Todays Leet-Code "94. Binary Tree Inorder Traversal" Solution with 2 approaches .

---
+++ Solution40.java
@@ -0,0 +1,44 @@
+//94. Binary Tree Inorder Traversal                                 09-12-2023 (Saturday)
+
+//Approch 1
+
+class Solution40 {
+    public List<Integer> inHelper(TreeNode root,List<Integer> list)
+    {
+        if(root == null)
+        return list;
+    
+        inHelper(root.left,list);
+    
+        list.add(root.val);
+    
+        inHelper(root.right,list);
+    
+        return list;
+    }
+    
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            return inHelper(root,list);
+        }
+    }
+
+
+
+//Approch 2
+
+// class Solution40 {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         helper(root, res);
+//         return res;
+//     }
+
+//     public void helper(TreeNode root, List<Integer> res) {
+//         if (root != null) {
+//             helper(root.left, res);
+//             res.add(root.val);
+//             helper(root.right, res);
+//         }
+//     }
+// }
