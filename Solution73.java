27b5a2b25ff5ac84f29f4f24101ee875d53aaf74
Vishal Shinde <95472421+Shinde2375@users.noreply.github.com>
2024-01-13 06:39:23 +0530

    1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ Solution73.java
@@ -0,0 +1,25 @@
+//1347. Minimum Number of Steps to Make Two Strings Anagram         12/01/2024(Saturday);
+
+import java.util.HashMap;
+
+public class Solution73 {
+
+        public int minSteps(String s, String t) {
+            HashMap<Character, Integer> h1 = new HashMap<>();
+    
+            for (char c : s.toCharArray()) {
+                h1.put(c, h1.getOrDefault(c, 0) + 1);
+            }
+    
+            int count = 0;
+            for (char c : t.toCharArray()) {
+                if (!h1.containsKey(c) || h1.get(c) == 0) {
+                    count++;
+                } else {
+                    h1.put(c, h1.get(c) - 1);
+                }
+            }
+    
+            return count;
+        }
+    }
